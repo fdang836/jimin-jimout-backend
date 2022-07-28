a5b886585261caa4ca1b467de1c5818899fe91a4
Fena Dang <fenadang@gmail.com>
2022-07-27 19:43:05 -0700

    initial commit

---
+++ src/test/java/bts/Capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package bts.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
