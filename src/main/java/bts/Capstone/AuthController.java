7723547a1afe6d977706f1d0a267e3c18509594a
Fena Dang <fenadang@gmail.com>
2022-08-03 20:56:05 -0700

    FD: Added comments

--- src/main/java/bts/Capstone/AuthController.java
+++ src/main/java/bts/Capstone/AuthController.java
@@ -33,50 +33,60 @@ import se.michaelthelin.spotify.model_objects.specification.Track;
 public class AuthController {
     @GetMapping("/api")
 	public ArrayList index() {
+        //Secret keys
         Dotenv dotenv = Dotenv.configure().load();
         String clientID = dotenv.get("CLIENT_ID");
         String clientSecret = dotenv.get("CLIENT_SECRET");
 
 
+        //Sending secret keys to Spotify API
         SpotifyApi spotifyApi = new SpotifyApi.Builder()
             .setClientId(clientID)
             .setClientSecret(clientSecret)
             .build();
         
-
+        //Building credentials
         ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
             .build();
 
 
         try {
+            //Get access token and set to Spotify API
             ClientCredentials clientCredentials = clientCredentialsRequest.execute();
             spotifyApi.setAccessToken(clientCredentials.getAccessToken());
             // System.out.println("Token " + clientCredentials.getAccessToken());
             // System.out.println("the api call worked");
 
+            //Get items within a playlist
             GetPlaylistsItemsRequest getPlaylistsItemsRequest = spotifyApi
                 .getPlaylistsItems("3W32s0YGRxVQGnQbCPMJb1")
                 .build();
             Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsItemsRequest.execute();
+
             ArrayList data = new ArrayList();
 
 
             for (int i = 0; i < 50; i++) {
                 Map<String, String> dictionary = new HashMap<String, String>();
+                //Accessing different values in nested data structure from get items within a playlist
                 String id = ((Track) playlistTrackPaging.getItems()[i].getTrack()).getId().toString();
                 String trackName = ((Track) playlistTrackPaging.getItems()[i].getTrack()).getName().toString();
                 String artist = ((Track) playlistTrackPaging.getItems()[i].getTrack()).getArtists()[0].getName().toString();
+
+                //Get the audio feature for each playlist
                 GetAudioFeaturesForTrackRequest getAudioFeaturesForTrackRequest = spotifyApi
                     .getAudioFeaturesForTrack(id)
                     .build();
                 AudioFeatures audioFeatures = getAudioFeaturesForTrackRequest.execute();
                 // System.out.println(audioFeatures.getValence());
                 String valenceData = audioFeatures.getValence().toString();
+                //Add data in hashmap 
                 dictionary.put("id", id);
                 dictionary.put("trackName", trackName);
                 dictionary.put("valence", valenceData);
                 dictionary.put("artist", artist);
 
+                //Add hashmap to arraylist to get returned
                 data.add(dictionary);
                 
             }
